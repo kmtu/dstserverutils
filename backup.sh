7317747a4aee04f971ad8a7a57ae7f5d6f7df4c5
KM Tu <me@kmtu.info>
2021-02-21 00:59:31 +0800

    feat: link the latest backup in backup folder

--- backup.sh
+++ backup.sh
@@ -5,5 +5,7 @@ do
     host=dst${shard}
     archive=$(ssh steam@${host} "bash -s ${world} ${shard}" < /home/kmtu/dst/mkarchive.sh)
     rsync -avh --remove-source-files steam@${host}:${archive} ${bkdir}/
+    cd ${bkdir}
+    ln -sf $(basename ${archive}) ${world}.${shard}.latest.tgz
 done
 
