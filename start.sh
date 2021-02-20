01d881e047e8376a3a02b10b8e05dd08541cd069
KM Tu <me@kmtu.info>
2021-02-20 17:07:40 +0800

    fix: don't start server if one already running

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@ for shard in master cave;
 do
     echo "starting ${shard}"
     host=dst${shard}
-    ssh steam@${host} 'tmux new -d -s dst ./run_dedicated_servers.sh'
+    ssh steam@${host} 'tmux has-session -t dst' && \
+        echo "dst already running on ${shard}" || \
+        ssh steam@${host} 'tmux new -d -s dst ./run_dedicated_servers.sh'
 done
 
