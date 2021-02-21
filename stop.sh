f0a63baca815e9b51461c03688340a4f8b6ab7ab
KM Tu <me@kmtu.info>
2021-02-21 22:48:54 +0800

    add allinone mode to stop.sh and wait_for_stop.sh

--- stop.sh
+++ stop.sh
@@ -1,8 +1,16 @@
-for shard in cave master;
-do
+mode="allinone" # or "separate"
+
+if [[ "$mode" == "separate" ]]; then
+  for shard in cave master;
+  do
     echo "stopping ${shard}"
     host=dst${shard}
     ssh steam@${host} 'tmux send-keys -t dst.0 "c_shutdown()" ENTER'
     ssh steam@${host} 'tmux send-keys -t dst.0 "c_shutdown()" ENTER'
-done
-
+  done
+elif [[ "$mode" == "allinone" ]]; then
+  echo "stopping server"
+  host=tuserver
+  ssh steam@${host} 'tmux send-keys -t dst-all.0 "c_shutdown()" ENTER'
+  ssh steam@${host} 'tmux send-keys -t dst-all.0 "c_shutdown()" ENTER'
+fi
--- wait_for_stop.sh
+++ wait_for_stop.sh
@@ -1,5 +1,8 @@
-for shard in cave master;
-do
+mode="allinone" # or "separate"
+
+if [[ "$mode" == "separate" ]]; then
+  for shard in cave master;
+  do
     host=dst${shard}
     scriptdir=/home/kmtu/dst
 
@@ -10,4 +13,15 @@ do
         ssh steam@${host} 'tmux has-session -t dst'
     done
     echo "${shard} stopped"
-done
+  done
+elif [[ "$mode" == "allinone" ]]; then
+  host=tuserver
+  scriptdir=/home/kmtu/dst
+  ssh steam@${host} 'tmux has-session -t dst-all'
+  while [[ "$?" == 0 ]]; do
+      echo "waiting for ${shard} to stop..."
+      sleep 5
+      ssh steam@${host} 'tmux has-session -t dst-all'
+  done
+  echo "${shard} stopped"
+fi
