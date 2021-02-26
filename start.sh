9e7cdfeee5105bf18539442e1d56a1773d2c602e
KM Tu <me@kmtu.info>
2021-02-26 08:08:37 +0800

    feat: add allinone mode to start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,19 @@
-for shard in master cave;
-do
+mode="allinone" # or "separate"
+
+if [[ "$mode" == "separate" ]]; then
+  for shard in master cave;
+  do
     echo "starting ${shard}"
     host=dst${shard}
     ssh steam@${host} 'tmux has-session -t dst' && \
-        echo "dst already running on ${shard}" || \
-        ssh steam@${host} 'tmux new -d -s dst ./run_dedicated_servers.sh'
-done
+      echo "dst already running on ${host}" || \
+      ssh steam@${host} 'tmux new -d -s dst ./run_dedicated_servers.sh'
+  done
+elif [[ "$mode" == "allinone" ]]; then
+  host=tuserver
+  echo "starting ${host}"
+  ssh steam@${host} 'tmux has-session -t dst-all' && \
+    echo "dst already running on ${host}" || \
+    ssh steam@${host} 'tmux new -d -s dst-all ./run_dedicated_servers-all.sh'
+fi
 
