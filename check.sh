2656e5b4a23dfad86d6fc674dc05ece77b9020a9
KM Tu <me@kmtu.info>
2021-03-21 23:04:23 +0800

    feat: add allinone mode to check.sh

--- check.sh
+++ check.sh
@@ -1,5 +1,8 @@
-for shard in master cave;
-do
+mode="allinone" # or "separate"
+
+if [[ "$mode" == "separate" ]]; then
+  for shard in master cave;
+  do
     host=dst${shard}
     scriptdir=/home/kmtu/dst
 
@@ -9,5 +12,15 @@ do
     else
         echo "${shard} is stopped"
     fi
-done
+  done
+elif [[ "$mode" == "allinone" ]]; then
+  host=dsttuserver
+  scriptdir=/home/kmtu/dst
 
+  ssh steam@${host} 'tmux has-session -t dst'
+  if [[ "$?" == 0 ]]; then
+      echo "server is running"
+  else
+      echo "server is stopped"
+  fi
+fi
