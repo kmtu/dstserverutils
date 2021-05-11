72fb9b052b136f1d695024cf1094662fb119f0a9
KM Tu <me@kmtu.info>
2021-05-12 01:33:31 +0800

    fix: check.sh for allinone

--- check.sh
+++ check.sh
@@ -4,8 +4,6 @@ if [[ "$mode" == "separate" ]]; then
   for shard in master cave;
   do
     host=dst${shard}
-    scriptdir=/home/kmtu/dst
-
     ssh steam@${host} 'tmux has-session -t dst'
     if [[ "$?" == 0 ]]; then
         echo "${shard} is running"
@@ -15,9 +13,7 @@ if [[ "$mode" == "separate" ]]; then
   done
 elif [[ "$mode" == "allinone" ]]; then
   host=dsttuserver
-  scriptdir=/home/kmtu/dst
-
-  ssh steam@${host} 'tmux has-session -t dst'
+  ssh steam@${host} 'tmux has-session -t dst-all'
   if [[ "$?" == 0 ]]; then
       echo "server is running"
   else
